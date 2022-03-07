efbd804741495825970900afba2d0492689f41d0
emmaofthevoid <tehurapardo@outlook.es>
2022-03-07 07:15:08 +0100

    added loop

---
+++ src/ForLoopPractice.java
@@ -0,0 +1,9 @@
+public class ForLoopPractice {
+    public static void main(String[] args) {
+        int number = 0;
+        for (int i = 0; i < 5; i++) {
+            System.out.println(number + i++);
+            number ++;
+        }
+    }
+}
